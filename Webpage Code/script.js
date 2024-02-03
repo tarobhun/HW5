c044470342aceac9492820c173b99fea51c291d8
Micah Coldiron <mcc4339@rit.edu>
2024-02-02 19:49:41 -0800

    first steps on the js done

--- Webpage Code/script.js
+++ Webpage Code/script.js
@@ -1,7 +1,11 @@
 import "jquery-3.7.1.min.js";
 $(document).ready(function{
-    
+    const chili = {name: "Mikaeyla's Famous Mushroom Chili", file: "Mikaeyla's Famous Mushroom Chili.txt", cuisine: "Mexican", time: "medium"};
+    const tsalad = {name: "Taco Salad", file: "Taco Salad.txt", cuisine: "Mexican", time: "short"};
+    const cbake = {name: "Chicken and Veggies Bake", file: "Chicken and Veggies Bake.txt", cuisine: "Fusion/American", time: "short"};
+    const pasta = {name: "Pasta with Mushroom Meat Sauce", file: "Pasta with Mushroom Meat Sauce.txt", cuisine: "Italian", time: "medium"};
+    let recipes = {chili, tsalad, cbake, pasta};
     $("placeholder").click(function {
-
+        let 
     });
 });
