c9552da2f6bb3db29045991e9bc50a448a7c8980
Micah Coldiron <mcc4339@rit.edu>
2024-02-03 13:57:28 -0800

    testing and bug fixing

--- Webpage Code/script.js
+++ Webpage Code/script.js
@@ -1,11 +1,23 @@
-import "jquery-3.7.1.min.js";
 $(document).ready(function(){
-    let recipes = {};
-    $("buttonAdd").click(function(){
-        let current = {name: $(".name").val(), cuisine: $(".select-cuisine").val(), meal: $(".select-meal").val(), recipe: $(".recipe").val()};
-        recipes.push(current);
+    let recipes = [];
+    $(".currentr").click(function() {
+        $("#home").css("visibility", "hidden");
+        $("#add").css("visibility", "hidden");
+        $("#recipes").css("visibility", "visible");
+        $("#textbox").append(recipes);
     });
-    $(".recipes").load(function() {
-        $(".content").text() = recipes;
+    $(".newr").click(function() {
+        $("#home").css("visibility", "hidden");
+        $("#add").css("visibility", "visible");
+        $("#recipes").css("visibility", "hidden");
+        $("#buttonAdd").click(function(){
+            let current = {name: $(".name").val(), cuisine: $(".select-cuisine").val(), meal: $(".select-meal").val(), recipe: $(".recipe").val()};
+            recipes.push(current);
+        });
+    });
+    $(".mainpage").click(function() {
+        $("#home").css("visibility", "visible");
+        $("#add").css("visibility", "hidden");
+        $("#recipes").css("visibility", "hidden");
     });
 });
